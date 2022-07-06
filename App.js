6e46773d68759f6c1e784071b79f2dee332e56ee
PimhathaiMeesomrote <pmeesomrote@gmail.com>
2022-07-06 08:53:50 +0700

    second commit

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Logo from './components/Logo'
 import User from './components/User'
+import LotsOfGreetings from './components/LotsOfGreetings'
 
 const App = () => {
   return (
     <View>
-      <Logo/>
-      <User/>
+      {/* <Logo/> */}
+      {/* <User/> */}
+      <LotsOfGreetings/>
     </View>
   )
 }
